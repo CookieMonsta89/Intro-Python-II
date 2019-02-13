81a56c38e480458582d83141b7b7e6221c258c17
CookieMonsta89 <jlstan06@hotmail.com>
2019-02-13 10:55:27 -0500

    started on player class

--- src/player.py
+++ src/player.py
@@ -2,5 +2,9 @@
 # currently.
 
 class Player:
-    def __init__(self, name, weapon):
-        self.weapon = weapon
+    def __init__(self, name, inventory):
+        self.name = name
+        self.inventory = inventory
+
+    def __str__(self):
+        return f"I believe my name is {self.name} and I seem to be weilding what looks to be a {self.inventory}"
--- src/room.py
+++ src/room.py
@@ -5,3 +5,6 @@ class Room:
     def __init__(self, room):
         self.room = room
 
+    def speak(self, room):
+        if room == outside 
+        print(f"Outside Cave Entrance","North of you, the cave mount beckons")
